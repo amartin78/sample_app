a1d5405fc84bf465e597dc64bfe75097e965d797
antonio <ammj24@hotmail.com>
2013-10-22 00:43:51 +0100

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Antonio Martin"
+    email "ammj24@hotmail.com"
+    password "fooggg"
+    password_confirmation "fooggg"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -103,5 +103,16 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
-  end  
+  end 
+  
+  describe "email address with mixed email" do
+    let(:mixed_case_email){ "fOo@exAmPle.cOM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+   
 end
